0d4f2be6eaacd5940f271fd684f7bc9f7e0833d9
byrone <ryanbyrone47@gmail.com>
2023-10-04 21:03:11 +0300

    Add index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+gsap.from('#one, #numberGroup path', {
+    stagger: {
+        each: 0.05
+    },
+    scaley:0,
+    delay: 0.6,
+    duration: 0.8,
+    transformOrigin: '50% 100%',
+    ease: 'elastic(0.73, 0.6)'
+})
